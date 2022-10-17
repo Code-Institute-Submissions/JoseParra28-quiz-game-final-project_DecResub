a5cfc37a00ad081e289fa93d2f1fd554a5e19c2e
JoseParra28 <iliftandcook@gmail.com>
2022-10-17 20:56:26 +0000

    add js for start button, timer, and start queistions and anwsers funtion

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,3 +3,54 @@ const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+let timeleft = 90;
+let downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Finished!!!";
+  } else {
+    document.getElementById("countdown").innerHTML = timeleft + "Seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+startButton.addEventListener('click', startGame)
+
+function setVisibility(id,Visibility){
+  document.getElementById(id).style.display=Visibility
+}
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
